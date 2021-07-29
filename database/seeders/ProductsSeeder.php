857cc39c7acd38daea67dc09ea9f4f8d082c0ef8
Malexander307 <malex307ander@gmail.com>
2021-07-29 23:33:41 +0300

    create proucts

---
+++ database/seeders/ProductsSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class ProductsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->insert([
+            'title' => Str::random(10),
+            'description' => Str::random(30),
+            'location' => Str::random(10),
+            'price' => rand(1, 25),
+        ]);
+    }
+}
