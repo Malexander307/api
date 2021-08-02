c8bcd3f5ebcae027909057258cd03e5c05cd5b64
Malexadner307 <o.moskalyk@grt-team.com>
2021-08-02 15:57:01 +0300

    a

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('products', ProductController::class);
+Route::resource('/products', ProductController::class);
