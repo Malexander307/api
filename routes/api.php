6ee8a5e696b68dea35f6f196f8fecdeb6b0cbcdb
Malexadner307 <o.moskalyk@grt-team.com>
2021-08-02 12:10:01 +0300

    some little changes

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Route::get('/products', [ProductController::class,'index']);
-//Route::post('/create-product', [ProductController::class,'store'])->name('create');
 Route::resource('products', ProductController::class);
